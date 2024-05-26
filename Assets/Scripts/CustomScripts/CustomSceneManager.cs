e05d6a336671cee00a2be085cd7fa5c9622c3336
Fuu <fukimax0612@gmail.com>
2024-05-26 14:21:50 +0900

    変数の一元管理

---
+++ Assets/Scripts/CustomScripts/CustomSceneManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomSceneManager : MonoBehaviour
+{
+    public static int swim;
+    public static int slip;
+    public static int run;
+    public static int fly;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Click()
+    {
+        SwimValue swimScript;
+        GameObject obj1 = GameObject.Find("TextSwim");
+        swimScript = obj1.GetComponent<SwimValue>();
+        swim = swimScript.swim;
+
+        SlipValue slipScript;
+        GameObject obj2 = GameObject.Find("TextSlip");
+        slipScript = obj2.GetComponent<SlipValue>();
+        slip = slipScript.slip;
+
+        RunValue runScript;
+        GameObject obj3 = GameObject.Find("TextRun");
+        runScript = obj3.GetComponent<RunValue>();
+        run = runScript.run;
+
+        FlyValue flyScript;
+        GameObject obj4 = GameObject.Find("TextFly");
+        flyScript = obj4.GetComponent<FlyValue>();
+        fly = flyScript.fly;
+
+        SceneManager.LoadScene("MainScene");
+    }
+}
